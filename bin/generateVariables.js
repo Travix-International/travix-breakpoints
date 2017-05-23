2c24fb3089a5b3b61fc280d2af0515f6fe281248
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-05-23 11:40:45 +0200

    use `eslint-config-travix`.

--- bin/generateVariables.js
+++ bin/generateVariables.js
@@ -1,26 +1,29 @@
 /* eslint-disable no-console */
 const fs = require('fs');
+const path = require('path');
 const _ = require('lodash');
 
 const VARIABLE_PREFIX = '--tx-breakpoint';
-const VARIABLES_FILE = __dirname + '/../lib/variables.js';
+const VARIABLES_FILE = path.join(__dirname, '../lib/variables.js');
 
 const sizes = require('../lib/sizes');
 const variables = {};
 
 _.each(sizes, function (size, name) {
-  const prefix = VARIABLE_PREFIX + '-' + name;
+  const prefix = `${VARIABLE_PREFIX}-${name}`;
   _.each(size, function (value, variable) {
-    variables[prefix + '-' + variable] = value;
+    variables[`${prefix}-${variable}`] = value;
   });
 
-  variables[prefix] = '(min-width: ' + size.min + ') and (max-width: ' + size.max + ')';
+  variables[prefix] = `(min-width: ${size.min}) and (max-width: ${size.max})`;
 });
 
 const existingContent = fs.readFileSync(VARIABLES_FILE, 'utf8');
-const writeContent = existingContent.split('*/\n')[0]
-  + '*/\n'
-  + 'module.exports = ' + JSON.stringify(variables, null, 2) + ';'
+const writeContent = [
+  existingContent.split('*/\n')[0],
+  '*/\n',
+  `module.exports = ${JSON.stringify(variables, null, 2)};\n`,
+].join('');
 
 fs.writeFileSync(VARIABLES_FILE, writeContent);
-console.log('File written at: ' + VARIABLES_FILE);
+console.log(`File written at: ${VARIABLES_FILE}`);
