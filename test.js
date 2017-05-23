0ba12b9c0c435d3a780b92ef421026c84ec09ff7
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-05-23 12:01:45 +0200

    add more tests.

--- test.js
+++ test.js
@@ -3,5 +3,19 @@ const _ = require('lodash');
 
 const lib = require('./');
 
+// variables
 assert.ok(_.isPlainObject(lib.variables));
+
+
+// sizes
 assert.ok(_.isPlainObject(lib.sizes));
+_.each(lib.sizes, function (entries, sizeName) {
+  const entryKeys = Object.keys(entries);
+
+  assert.ok(entryKeys.length > 1);
+
+  const hasMin = entryKeys.indexOf('min') > -1;
+  const hasMax = entryKeys.indexOf('max') > -1;
+
+  assert.ok(hasMin || hasMax);
+});
