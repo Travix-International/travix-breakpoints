345e97029661260764049c5085e60e24f8bd91d7
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-05-24 18:21:53 +0200

    Support CSS Modules spec for variables. (#2)

--- bin/generateVariables.js
+++ bin/generateVariables.js
@@ -4,26 +4,54 @@ const path = require('path');
 const _ = require('lodash');
 
 const VARIABLE_PREFIX = '--tx-breakpoint';
-const VARIABLES_FILE = path.join(__dirname, '../lib/variables.js');
+const VARIABLES_JS_FILE = path.join(__dirname, '../variables.js');
+const VARIABLES_CSS_FILE = path.join(__dirname, '../variables.css');
 
-const sizes = require('../lib/sizes');
-const variables = {};
+const sizes = require('../sizes');
+const jsVars = {};
+const cssVarsLines = [];
 
+
+/**
+ * Prepare variables
+ */
 _.each(sizes, function (size, name) {
+
   const prefix = `${VARIABLE_PREFIX}-${name}`;
   _.each(size, function (value, variable) {
-    variables[`${prefix}-${variable}`] = value;
+    jsVars[`${prefix}-${variable}`] = value;
+
+    const cssVarName = _.camelCase(`${name}-${variable}`);
+    cssVarsLines.push(`@value ${cssVarName}: ${value};`);
   });
 
-  variables[prefix] = `(min-width: ${size.min}) and (max-width: ${size.max})`;
+  const condition = `(min-width: ${size.min}) and (max-width: ${size.max})`;
+  jsVars[prefix] = condition;
+  cssVarsLines.push(`@value ${name}: ${condition};`);
 });
 
-const existingContent = fs.readFileSync(VARIABLES_FILE, 'utf8');
-const writeContent = [
-  existingContent.split('*/\n')[0],
+
+/**
+ * Write to file
+ */
+// JS
+const existingJsContent = fs.readFileSync(VARIABLES_JS_FILE, 'utf8');
+const writeJsContent = [
+  existingJsContent.split('*/\n')[0],
   '*/\n',
-  `module.exports = ${JSON.stringify(variables, null, 2)};\n`,
+  `module.exports = ${JSON.stringify(jsVars, null, 2)};\n`,
 ].join('');
 
-fs.writeFileSync(VARIABLES_FILE, writeContent);
-console.log(`File written at: ${VARIABLES_FILE}`);
+fs.writeFileSync(VARIABLES_JS_FILE, writeJsContent);
+console.log(`File written at: ${VARIABLES_JS_FILE}`);
+
+// CSS
+const existingCssContent = fs.readFileSync(VARIABLES_CSS_FILE, 'utf8');
+const writeCssContent = [
+  existingCssContent.split('*/\n')[0],
+  '*/\n',
+  `${cssVarsLines.join('\n')}\n`,
+].join('');
+
+fs.writeFileSync(VARIABLES_CSS_FILE, writeCssContent);
+console.log(`File written at: ${VARIABLES_CSS_FILE}`);
